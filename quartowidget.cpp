05e10931716f907cb2ea8d98c850805f3bf1bc2e
Bouba_BARRY <boubasidy18@gmail.com>
2025-03-21 14:30:00 +0100

    Added piece button grid and placement functionality in QuadrantWidget

--- quartowidget.cpp
+++ quartowidget.cpp
@@ -23,3 +23,16 @@ QuadrantWidget::QuadrantWidget(QWidget *parent)
     piecesLayout = new QHBoxLayout;
     QGridLayout *boardLayout = new QGridLayout;
     statusLabel = new QLabel(QString::fromStdString(_status));
+
+    for (int i = 0; i < 16; ++i) {
+        QPushButton *pieceButton = new QPushButton;
+        pieceButton->setFixedSize(60,60);
+        //pieceButton->setIcon(QIcon(pix));
+        pieceButton->setIconSize(pix.size());
+
+        int row = floor(i / 4);
+        int col = i % 4;
+        boardLayout->addWidget(pieceButton, row, col);
+        //connect(pieceButton, &QPushButton::clicked, this, &quatrowidget::Put_Piece);
+        connect(pieceButton, &QPushButton::clicked, this, [this, row, col]() {Place_Piece(row, col);});
+    }
