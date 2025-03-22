1d9009481b20453225ad3e1ea88f4d051fc2984e
Bouba_BARRY <boubasidy18@gmail.com>
2025-03-22 16:00:00 +0100

    Added CLAIRE_PETIT_ROND_TROU button with rendering and selection handling

--- quartowidget.cpp
+++ quartowidget.cpp
@@ -36,3 +36,25 @@ QuadrantWidget::QuadrantWidget(QWidget *parent)
         //connect(pieceButton, &QPushButton::clicked, this, &quatrowidget::Put_Piece);
         connect(pieceButton, &QPushButton::clicked, this, [this, row, col]() {Place_Piece(row, col);});
     }
+
+    QPixmap pix(50, 50);
+    pix.fill(Qt::transparent);
+    QPainter painter(&pix);
+    painter.setPen(QColor(31,31,31));
+
+
+
+    QPushButton *CLAIRE_PETIT_ROND_TROU = new QPushButton;
+    painter.setBrush(QColor(253, 245, 172));
+    painter.drawEllipse(14, 14, 22, 22);
+    painter.setBrush(QColor(51, 25, 0));
+    painter.drawEllipse(23, 23, 4, 4);
+    CLAIRE_PETIT_ROND_TROU->setIcon(QIcon(pix));
+    CLAIRE_PETIT_ROND_TROU->setIconSize(pix.size());
+    piecesLayout->addWidget(CLAIRE_PETIT_ROND_TROU);
+    CLAIRE_PETIT_ROND_TROU->setObjectName("CLAIRE-PETIT-ROND-TROU");
+    connect(CLAIRE_PETIT_ROND_TROU, &QPushButton::clicked, this, &QuadrantWidget::Choose_Piece);
+    connect(CLAIRE_PETIT_ROND_TROU, &QPushButton::clicked, this, [=]() {
+            CLAIRE_PETIT_ROND_TROU->setStyleSheet("background-color: green;");
+        });
+    CLAIRE_PETIT_ROND_TROU->installEventFilter(this);
